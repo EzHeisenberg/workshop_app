849b77f037167acce28547621594ca21237bdea7
Jmmpoy <dunker35@hotmail.fr>
2022-11-09 11:23:52 +0100

    Dashboard Card items

--- src/partials/Card.jsx
+++ src/partials/Card.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function CustomCard({
-    title, content, heightValue
-}) {
-
-
+function CustomCard({ title, content, heightValue }) {
   return (
-      <div style={{height: {heightValue}}} className="flex flex-col bg-base-100 shadow-lg rounded-sm border border-slate-200">
-        <header className="px-5 py-4 border-b border-slate-100">
-          <h2 className="font-semibold text-slate-800">{title}</h2>
-        </header>
-        <div className="p-4">
-          {content}
-        </div>
-      </div>
+    <div
+      style={{ height: { heightValue } }}
+      className="flex flex-col col-span-6 bg-base-100 shadow-lg rounded-sm border border-slate-200">
+      <header className="px-5 py-4 border-b border-slate-100">
+        <h2 className="font-semibold text-slate-800">{title}</h2>
+      </header>
+      <div className="p-4">{content}</div>
+    </div>
   );
 }
 
